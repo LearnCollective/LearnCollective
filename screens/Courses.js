477178c4cc62afd3d972e3f88e2e6ff42d7f4f1d
mina <mena24344@gmail.com>
2023-04-05 14:02:45 +0200

    add Screens

---
+++ screens/Courses.js
@@ -0,0 +1,14 @@
+import react from "react";
+import {
+    View,
+    Text
+} from 'react-native';
+export default function Courses() {
+    return (
+        <View>
+            <Text>Welcome in Courses</Text>
+        </View>
+
+
+    );
+}
---
+++ screens/Profile.js
@@ -0,0 +1,68 @@
+import react from "react";
+import { Text, View, StyleSheet, Button, ImageBackground, Dimensions } from "react-native";
+import { signOut } from "firebase/auth";
+import { globalstyles } from "../styles/global";
+import { auth } from "../firebase/firebase_config";
+export default function Profile({ navigation }) {
+    const press = () => {
+        navigation.push('SIGNIN');
+
+    }
+    const signOuthandle = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            navigation.push('SIGNIN');
+
+        }).catch((error) => {
+            // An error happened.
+        });
+    }
+    return (
+        <ImageBackground
+            source={require('../assets/back2.png')}
+            style={{
+                height: Dimensions.get('window').height
+            }}>
+            <View style={globalstyles.container}>
+
+                <ImageBackground style={styles.photo}  >
+                    <Button onPress={press} title="SignOut"></Button>
+
+
+                </ImageBackground>
+
+            </View>
+
+        </ImageBackground>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+
+    }, photo: {
+        justifyContent: 'center',
+        marginLeft: -24,
+        marginTop: -24,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+
+    },
+    text: {
+        textAlign: 'center'
+        , fontSize: 30,
+        color: 'white',
+        fontStyle: 'italic'
+
+    }, Tbtn: {
+        marginLeft: 15,
+    }, teext: {
+        backgroundColor: 'blue'
+        , color: '#fff',
+        padding: 10,
+    }
+    , cont: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    }
+
+});
