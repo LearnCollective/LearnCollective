669015190d3ec8ef5032f959aedc8bc0f4ac721e
mina <mena24344@gmail.com>
2023-05-02 00:18:34 +0300

    edit register

--- screens/Register.js
+++ screens/Register.js
@@ -1,20 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, alert, TouchableOpacity, Button, Text, Item, ScrollView, View, TextInput, ImageBackground, Dimensions, Image } from 'react-native';
+import { StyleSheet, alert, TouchableOpacity, Button, Text, Item, ScrollView, View, TextInput, ImageBackground, Dimensions, Image, Alert } from 'react-native';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, provider } from "../firebase/firebase_config";
+import { auth, provider, db } from "../firebase/firebase_config";
 import { CheckBox, Input } from 'react-native-elements';
 import photo2 from '../assets/back.jpg'
 import { ListItem, SocialIcon } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Formik } from 'formik';
-import * as yup from 'yup'
-
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import * as yup from 'yup';
+import PhoneInput from "react-native-phone-number-input";
+import { doc, setDoc } from "firebase/firestore";
+import { getAuth, signInWithPopup } from "firebase/auth";
 import { marginTop, paddingBottom, width } from 'styled-system';
 
 
 const loginValidationSchema = yup.object().shape({
+  fullname: yup.string().matches(/(\w.+\s).+/, 'Enter at least 2 names').required('Full name is required'),
   email: yup.string().email('Please enter valid email').required('Email Address is required'),
+  dateofbirth: yup.string()
+    .required('Date of Birth is required')
+    .matches(/^\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$/, 'Date of Birth must be a valid date in the format YYYY-MM-DD'),
+  phonenumber: yup.string().matches(/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/, 'Phone number is not valid').required('Phone Number is required'),
   password: yup.string()
     .required('password is required').matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
@@ -26,17 +32,30 @@ const loginValidationSchema = yup.object().shape({
 
 const image = { uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBR9MAw997U9x7Vjx0uQ4HFmMW9MM8ftj1NA&usqp=CAU' };
 export default function Register({ navigation }) {
-  const provider = new GoogleAuthProvider();
   const [ShowPassword, setShowPassword] = useState(false);
   const [ShowconfirmPassword, setShowconfirmPassword] = useState(false);
   const [rememberMe, setrememberme] = useState(false);
+
+
   const press = () => {
     navigation.navigate('SIGNIN');
   }
   const press2 = () => {
     navigation.navigate('Home');
   }
-  function SignGoogle () {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [,] = useState([]);
+
+  const storeDataBase = async (email, name, phone, date) => {
+    await setDoc(doc(db, "users", auth.currentUser.uid), {
+      email: email,
+      name: name,
+      phone: phone,
+      birthdate: date
+    });
+  };
+  // const auth = getAuth();
+  const handelSignGoogle = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
@@ -46,6 +65,7 @@ export default function Register({ navigation }) {
         const user = result.user;
         // IdP data available using getAdditionalUserInfo(result)
         // ...
+
       }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
@@ -62,26 +82,35 @@ export default function Register({ navigation }) {
 
   const increment = () => {
     setcont(count + 1);
-  }
+  };
+
   return (
 
     <Formik
 
-      initialValues={{ email: '', password: '', confirmPwd: '' }}
+      initialValues={{ fullname: '', email: '', dateofbirth: '', phonenumber: '', password: '', confirmPwd: '' }}
       validateOnMount={true}
       onSubmit={values => {
         ////call sign In her
-        createUserWithEmailAndPassword(auth, values.email, values.password)
+        createUserWithEmailAndPassword(auth, values.email, values.password, values.fullname, values.dateofbirth, values.phonenumber)
 
           .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
             // ...
-            navigation.navigate('Onboard');
+            storeDataBase(values.email, values.fullname, values.phonenumber, values.dateofbirth);
+
+            console.log(values.email)
+            navigation.navigate('Onboard')
+
+            // {
+            //   paramKey: values.fullname,
+            // }
           })
           .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
+            console.log("error")
             // ..
           });
 
@@ -133,6 +162,24 @@ export default function Register({ navigation }) {
               </Text>
               {/*Form inputs View */}
               <View style={{ marginTop: 50 }}>
+
+                <View floatingLable style={{ borderColor: '#4632A1', alignItems: 'stretch' }}>
+                  <Text>Full Name</Text>
+                  <View style={{ flexDirection: 'row' }}>
+                    <Input style={styles.bord} fontWeight='500'
+                      onChangeText={handleChange('fullname')}
+                      onBlur={handleBlur('fullname')} value={values.fullname}
+                    />
+                    <Icon name={!errors.fullname ? 'check' : 'close'} style={{ color: !errors.fullname ? '#4632A1' : 'red' }} />
+
+                  </View>
+                </View>
+                <View style={{ marginTop: -10, paddingBottom: 10 }}>
+                  {[errors.fullname && touched.fullname] &&
+                    <Text style={styles.errors}>{errors.fullname}</Text>
+                  }
+                </View>
+
                 <View floatingLable style={{ borderColor: '#4632A1' }}>
                   <Text>Email</Text>
                   <View style={{ flexDirection: 'row', alignItems: 'stretch' }}>
@@ -149,8 +196,42 @@ export default function Register({ navigation }) {
                   {[errors.email && touched.email] &&
                     <Text style={styles.errors}>{errors.email}</Text>
                   }
-
                 </View>
+
+                <View floatingLable style={{ borderColor: '#4632A1' }}>
+                  <Text>Date Of Birth</Text>
+                  <View style={{ flexDirection: 'row', alignItems: 'stretch' }}>
+
+                    <Input onChangeText={handleChange('dateofbirth')}
+                      onBlur={handleBlur('dateofbirth')} value={values.dateofbirth}
+                    />
+
+                    <Icon name={!errors.dateofbirth ? 'check' : 'close'} style={{ color: !errors.dateofbirth ? '#4632A1' : 'red' }} />
+                  </View>
+                </View>
+                <View style={{ marginTop: -10, paddingBottom: 10 }}>
+                  {[errors.dateofbirth && touched.dateofbirth] &&
+                    <Text style={styles.errors}>{errors.dateofbirth}</Text>
+                  }
+                </View>
+
+                <View floatingLable style={{ borderColor: '#4632A1' }}>
+                  <Text>Phone Number</Text>
+                  <View style={{ flexDirection: 'row', alignItems: 'stretch' }}>
+                    <PhoneInput
+                      defaultCode='EG'
+                      onChangeText={handleChange('phonenumber')}
+                      onBlur={handleBlur('phonenumber')} value={values.phonenumber}
+                    />
+                    <Icon top={22} name={!errors.phonenumber ? 'check' : 'close'} style={{ color: !errors.phonenumber ? '#4632A1' : 'red' }} />
+                  </View>
+                </View>
+                <View style={{ marginTop: -10, paddingBottom: 10 }}>
+                  {[errors.phonenumber && touched.phonenumber] &&
+                    <Text style={styles.errors}>{errors.phonenumber}</Text>
+                  }
+                </View>
+
                 <View floatingLable style={{ borderColor: '#4632A1', alignItems: 'stretch' }}>
                   <Text>Password</Text>
                   <View style={{ flexDirection: 'row' }}>
@@ -226,8 +307,7 @@ export default function Register({ navigation }) {
                     <SocialIcon button type='twitter' />
                   </TouchableOpacity>
 
-
-                  <TouchableOpacity onPress={SignGoogle} Icon style={[styles.shadowBtn, { backgroundColor: '#dd4a39' }, { width: 55, height: 65 }]} rounded >
+                  <TouchableOpacity onPress={handelSignGoogle} Icon style={[styles.shadowBtn, { backgroundColor: '#dd4a39' }, { width: 55, height: 65 }]} rounded >
                     <SocialIcon button type='google' />
                   </TouchableOpacity>
 
@@ -239,9 +319,10 @@ export default function Register({ navigation }) {
           </View>
 
         </ScrollView>
-      )}
+      )
+      }
 
-    </Formik>
+    </Formik >
 
   )
 }
@@ -289,12 +370,8 @@ const styles = StyleSheet.create({
     color: 'red',
     fontWeight: 'bold'
     , marginTop: 5,
-  }
-
-
-
-
-  , container: {
+  },
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
