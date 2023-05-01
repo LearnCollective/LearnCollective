d9595fd6223df1dfa4061a43be3f71d226824802
hazemAmr0 <109316536+hazemAmr0@users.noreply.github.com>
2023-05-01 21:16:58 +0300

    complete setting profile

--- routes/homestack.js
+++ routes/homestack.js
@@ -1,64 +1,66 @@
-import { createStackNavigator, Header, HeaderTitle } from "react-navigation-stack";
+import {
+  createStackNavigator,
+  Header,
+  HeaderTitle,
+} from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 import Login from "../screens/Login";
 import Home from "../screens/home";
 import Register from "../screens/Register";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import forgetpassword from "../screens/Forgetpassword";
 import OnboardingScreen from "../screens/OnboardingScreen";
 import Profile from "../screens/Profile";
+import editprofile from "../screens/editprofile";
 
-const screens={
-  SIGNIN:{
-    screen:Login,
+const screens = {
+  editprof: {
+    screen: editprofile,
     navigationOptions: ({ navigation }) => ({
-        headerShown: navigation.getParam('showHeader', false),
-      }),
-},
-
-REGISTER:{
-    screen:Register,
+      headerShown: navigation.getParam("showHeader", false),
+    }),
+  },
+  PROFILE: {
+    screen: Profile,
     navigationOptions: ({ navigation }) => ({
-        headerShown: navigation.getParam('showHeader', false),
-      }),   
-}, 
- 
- 
-
-
-   
-   
-    Home:{
-        screen:Home,
-        navigationOptions: ({ navigation }) => ({
-            headerShown: navigation.getParam('showHeader', false),
-          }),
-    },
-
-    PROFILE:{
-      screen:Profile,
-      navigationOptions: ({ navigation }) => ({
-          headerShown: navigation.getParam('showHeader', false),
-        }),
+      headerShown: navigation.getParam("showHeader", false),
+    }),
   },
-    
-    Onboard:{
-      screen:OnboardingScreen,
-      navigationOptions: ({ navigation }) => ({
-          headerShown: navigation.getParam('showHeader', false),
-        }),   
-  }, 
- Forgetpassword:{ 
-      screen:forgetpassword,
-      navigationOptions: ({ navigation }) => ({
-          headerShown: navigation.getParam('showHeader', false),
-        }),
+
+  REGISTER: {
+    screen: Register,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: navigation.getParam("showHeader", false),
+    }),
   },
-   
 
-    
-}
-const HomeStack=createStackNavigator(screens);
+  Onboard: {
+    screen: OnboardingScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: navigation.getParam("showHeader", false),
+    }),
+  },
 
+  Home: {
+    screen: Home,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: navigation.getParam("showHeader", false),
+    }),
+  },
+  SIGNIN: {
+    screen: Login,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: navigation.getParam("showHeader", false),
+    }),
+  },
+
+  Forgetpassword: {
+    screen: forgetpassword,
+    navigationOptions: ({ navigation }) => ({
+      headerShown: navigation.getParam("showHeader", false),
+    }),
+  },
+};
+const HomeStack = createStackNavigator(screens);
 
 export default createAppContainer(HomeStack);
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,73 +1,271 @@
 import react from "react";
-import { Text, View, StyleSheet, Button, ImageBackground, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  ImageBackground,
+  Dimensions,
+  TextInput,
+  SafeAreaView,
+  Image,
+} from "react-native";
 import { signOut } from "firebase/auth";
 import { globalstyles } from "../styles/global";
 import { auth } from "../firebase/firebase_config";
+import React, { useState } from "react";
+import { TouchableOpacity, Switch } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Modal from "react-native-modal";
+import ImagePicker from "react-native-image-picker";
+
+//--------------------------------------------------------------------------------------------------
 export default function Profile({ navigation }) {
-    // const signOuthandle = () => {
-    //     signOut(auth).then(() => {
-    //         // Sign-out successful.
-    //         navigation.navigate('SIGNIN');
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [firstName, setFirstName] = useState("hazem");
+  const [lastName, setLastName] = useState("hgjf@gmail.com");
+  const [email, setEmail] = useState("hazemamromar@gmail.com");
+  const [birthdate, setBirthdate] = useState("1/1/1990");
+  const [phoneNumber, setPhoneNumber] = useState("01158885124");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
 
-    //     }).catch((error) => {
-    //         // An error happened.
-    //     });
-    // }
-     const signOuthandle = () => {
-        signOut(auth).then(() => {
-          // Sign-out successful.
-          navigation.navigate('SIGNIN');
-    
-        }).catch((error) => {
-          // An error happened.
-        });
-      }
-    return (
-        <ImageBackground
-            source={require('../assets/back2.png')}
-            style={{
-                height: Dimensions.get('window').height
-            }}>
-            <View style={globalstyles.container}>
+  //--------------------------------validation-------------------------------------------------------------
+  const validateForm = () => {
+    const errors = {};
 
-                <ImageBackground style={styles.photo}  >
-                    <Button onPress={signOuthandle} title="SignOut"></Button>
+    // Validate first name
+    if (!firstName) {
+      errors.firstName = "First name is required";
+    }
 
+    // Validate last name
+    if (!lastName) {
+      errors.lastName = "Last name is required";
+    }
 
-                </ImageBackground>
+    // Validate email
+    if (!email) {
+      errors.email = "Email is required";
+    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
+      errors.email = "Invalid email address";
+    }
 
-            </View>
+    // Validate birthdate
+    if (!birthdate) {
+      errors.birthdate = "Birthdate is required";
+    }
 
-        </ImageBackground>
-    );
+    // Validate phone number
+    if (!phoneNumber) {
+      errors.phoneNumber = "Phone number is required";
+    } else if (!/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(phoneNumber)) {
+      errors.phoneNumber = "Invalid phone number";
+    }
+
+    setValidationErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
+  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  const handleSaveProfile = () => {
+    const isFormValid = validateForm();
+
+    if (isFormValid) {
+      // TODO: handle saving profile changes to backend
+      //  setIsEditMode(false);
+    } else {
+      setIsSubmitted(true);
+      setIsEditMode(false);
+    }
+  };
+
+  const renderViewMode = () => (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/profile.png")}
+        style={styles.provileAvatar}
+      />
+      <Text style={styles.label}>Name:</Text>
+      <Text style={styles.value}>
+        {firstName} {lastName}
+      </Text>
+      <Text style={styles.label}>Email:</Text>
+      <Text style={[styles.value, styles.dimmedValue]}>{email}</Text>
+      <Text style={styles.label}>Birthdate:</Text>
+      <Text style={styles.value}>{birthdate}</Text>
+      <Text style={styles.label}>Phone Number:</Text>
+      <Text style={styles.value}>{phoneNumber}</Text>
+      <TouchableOpacity
+        onPress={() => setIsEditMode(true)}
+        style={styles.editButton}
+      >
+        <Text style={styles.editButtonText}>Edit Profile</Text>
+      </TouchableOpacity>
+    </View>
+  );
+  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  const renderEditMode = () => (
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/profile.png")}
+        style={styles.provileAvatar}
+      />
+
+      <TextInput
+        value={firstName}
+        onChangeText={setFirstName}
+        placeholder="First Name"
+        style={styles.textInput}
+      />
+      {firstName.length < 3 && (
+        <Text style={styles.validationError}>First name is too short.</Text>
+      )}
+      <TextInput
+        value={lastName}
+        onChangeText={setLastName}
+        placeholder="Last Name"
+        style={styles.textInput}
+      />
+      {lastName.length < 3 && (
+        <Text style={styles.validationError}>Last name is too short.</Text>
+      )}
+      <Text style={styles.label}>Email:</Text>
+      <Text style={[styles.value, styles.dimmedValue]}>{email}</Text>
+      <TextInput
+        value={birthdate}
+        onChangeText={setBirthdate}
+        placeholder="Birthdate (MM/DD/YYYY)"
+        style={styles.textInput}
+      />
+      {!/^([1-9]|1[0-2])\/([1-9]|[12]\d|3[01])\/\d{4}$/.test(birthdate) && (
+        <Text style={styles.validationError}>
+          Birthdate format must be MM/DD/YYYY.
+        </Text>
+      )}
+      <TextInput
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+        placeholder="Phone Number"
+        style={styles.textInput}
+      />
+      {!/^\d{3}\d{3}\d{4}\d{1}$/.test(phoneNumber) && (
+        <Text style={styles.validationError}>
+          Phone number format must be 11 number.
+        </Text>
+      )}
+      <TouchableOpacity onPress={handleSaveProfile} style={styles.saveButton}>
+        <Text style={styles.saveButtonText}>Save Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => setIsEditMode(false)}
+        style={styles.cancelButton}
+      >
+        <Text style={styles.cancelButtonText}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+  return <View>{isEditMode ? renderEditMode() : renderViewMode()}</View>;
 }
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 const styles = StyleSheet.create({
-    container: {
-
-    }, photo: {
-        justifyContent: 'center',
-        marginLeft: -24,
-        marginTop: -24,
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-
-    },
-    text: {
-        textAlign: 'center'
-        , fontSize: 30,
-        color: 'white',
-        fontStyle: 'italic'
-
-    }, Tbtn: {
-        marginLeft: 15,
-    }, teext: {
-        backgroundColor: 'blue'
-        , color: '#fff',
-        padding: 10,
-    }
-    , cont: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-    }
-
+  validationError: {
+    color: "red", // Add this line to change the color to red
+    marginBottom: 10,
+  },
+  container: {
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+  profilePhoto: {
+    width: 100,
+    height: 100,
+    marginBottom: 17,
+    borderRadius: 50,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 7,
+    backgroundColor: "#A5A7AA",
+    borderRadius: 8,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  value: {
+    fontSize: 18,
+    marginBottom: 10,
+    marginBottom: 10,
+    backgroundColor: "#ECEAEBEF",
+    borderRadius: 3,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  dimmedValue: {
+    color: "#999",
+  },
+  editButton: {
+    marginTop: 16,
+    backgroundColor: "#0C1116",
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  editButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  textInput: {
+    fontSize: 18,
+    marginBottom: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+    paddingVertical: 8,
+  },
+  saveButton: {
+    marginTop: 16,
+    backgroundColor: "#0C1722",
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  saveButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  cancelButton: {
+    marginTop: 16,
+    backgroundColor: "#3D628D",
+    borderRadius: 12,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cancelButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  provileAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 9999,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginRight: 12,
+  },
 });
