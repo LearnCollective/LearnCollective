0da97a7cad2a0108a62a64542e3f0982f64f92fa
ahmed111111111110 <ahmedhuseeny1@gmail.com>
2023-05-10 17:17:47 +0300

    Home tab edit

--- screens/HomeTab.js
+++ screens/HomeTab.js
@@ -8,6 +8,10 @@ import DownloadCourses from "./DownloadCourses";
 import Profile from "./Profile";
 import { View, Text, StyleSheet, ImageBackground, Dimensions } from "react-native";
 import { AntDesign, Ionicons, SimpleLineIcons } from '@expo/vector-icons';
+import Home from "./home";
+import Cart from './Cart'
+
+
 // import { Ionicons } from "@expo/vector-icons";
 const Tab = createBottomTabNavigator();
 
@@ -15,102 +19,101 @@ export default function HomeTab() {
 
     return (
 
-        <NavigationContainer>
-            <Tab.Navigator
-                tabBarOptions={{
-                    showLabel: false,
-                }}
-                screenOptions={
-                    {
-                        tabBarStyle: {
-                            position: 'absolute',
-                            bottom: 15,
-                            left: 20,
-                            right: 20,
-                            borderRadius: 20,
-                            shadowColor: 'blue',
-                            shadowRadius: 3.5,
-                        },
-                    }} >
+        <Tab.Navigator
+        tabBarOptions={{
+            showLabel: false,
+        }}
+        screenOptions={
+            {
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 15,
+                    left: 20,
+                    right: 20,
+                    borderRadius: 20,
+                    shadowColor: 'blue',
+                    shadowRadius: 3.5,
+                },
+            }} >
+        <Tab.Screen name="Home" component={Home}
+
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <View style={{ alignItems: 'center' }}>
+                        <AntDesign
+                            name="home"
+                            size={25}
+                            color={focused ? 'gray' : 'blue'}
+                        >
+                        </AntDesign>
+                        <Text style={{
+                            alignItems: 'center',
+                            color: focused ? 'gray' : 'blue',
+                        }}>Home</Text>
+
+                    </View>
+                ),
+                headerShown: false
+            }} />
+
+        <Tab.Screen name="search" component={Search} options={{
+            tabBarIcon: ({ focused }) => (
+                <View style={{ alignItems: 'center' }}>
+                    <AntDesign name="search1"
+                        size={20}
+                        color={focused ? 'gray' : 'blue'}
+                    >
+                    </AntDesign>
+                    <Text style={{
+                        alignItems: 'center',
+                        color: focused ? 'gray' : 'blue',
+                    }}>Search</Text>
+
+                </View>
+            ),
+            headerShown: false
+        }}
 
 
-                <Tab.Screen name="search" component={Search} options={{
-                    tabBarIcon: ({ focused }) => (
-                        <View style={{ alignItems: 'center' }}>
-                            <AntDesign name="search1"
-                                size={20}
-                                color={focused ? 'gray' : 'blue'}
-                            >
-                            </AntDesign>
-                            <Text style={{
-                                alignItems: 'center',
-                                color: focused ? 'gray' : 'blue',
-                            }}>Search</Text>
-
-                        </View>
-                    ),
-                    headerShown: false
-                }}
+        />
 
 
-                />
-                <Tab.Screen name="Courses" component={Courses}
+        <Tab.Screen name="Cart" component={Cart} options={{
+            tabBarIcon: ({ focused }) => (
+                <View style={{ alignItems: 'center' }}>
+                    <AntDesign name="shoppingcart"
+                        size={25}
+                        color={focused ? 'gray' : 'blue'}
+                    >
+                    </AntDesign>
+                    <Text style={{
+                        alignItems: 'center',
+                        color: focused ? 'gray' : 'blue',
+                    }}>Cart</Text>
 
-                    options={{
-                        tabBarIcon: ({ focused }) => (
-                            <View style={{ alignItems: 'center' }}>
-                                <SimpleLineIcons
-                                    name="book-open"
-                                    size={20}
-                                    color={focused ? 'gray' : 'blue'}
-                                >
-                                </SimpleLineIcons>
-                                <Text style={{
-                                    alignItems: 'center',
-                                    color: focused ? 'gray' : 'blue',
-                                }}>Courses</Text>
+                </View>
+            ), headerShown: false
+        }} />
+        <Tab.Screen name="Profile" component={Profile} options={{
+            tabBarIcon: ({ focused }) => (
+                <View style={{ alignItems: 'center' }}>
 
-                            </View>
-                        ),
-                        headerShown: false
-                    }} />
+                    <Ionicons name="ios-person-circle-outline"
+                        size={25}
+                        color={focused ? 'gray' : 'blue'}
+                    >
+                    </Ionicons>
 
-                <Tab.Screen name="Download" component={DownloadCourses} options={{
-                    tabBarIcon: ({ focused }) => (
-                        <View style={{ alignItems: 'center' }}>
-                            <SimpleLineIcons name="cloud-download"
-                                size={20}
-                                color={focused ? 'gray' : 'blue'}
-                            >
-                            </SimpleLineIcons>
-                            <Text style={{
-                                alignItems: 'center',
-                                color: focused ? 'gray' : 'blue',
-                            }}>Downloads</Text>
+                    <Text style={{
+                        alignItems: 'center',
+                        color: focused ? 'gray' : 'blue',
+                        marginTop: -4,
+                    }}>Profile</Text>
+                </View>
+            ), headerShown: false
+        }} />
+    </Tab.Navigator >
+    // </NavigationContainer>
 
-                        </View>
-                    ), headerShown: false
-                }} />
-                <Tab.Screen name="Profile" component={Profile} options={{
-                    tabBarIcon: ({ focused }) => (
-                        <View style={{ alignItems: 'center' }}>
-
-                            <Ionicons name="ios-person-circle-outline"
-                                size={25}
-                                color={focused ? 'gray' : 'blue'}
-                            >
-                            </Ionicons>
-
-                            <Text style={{
-                                alignItems: 'center',
-                                color: focused ? 'gray' : 'blue',
-                                marginTop: -4,
-                            }}>Profile</Text>
-                        </View>
-                    ), headerShown: false
-                }} />
-            </Tab.Navigator >
-        </NavigationContainer>
-
-    );
+);
 }
